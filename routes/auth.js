aaa778793b1a5594cffdd2811e3dd17cfbe91423
bethqiang <bethany.qiang@gmail.com>
2017-05-01 17:22:07 -0500

    Replace passport-local strategy w homegrown strategy
    - Won't be using Passport for OAuth, so I don't see much of a point ot use a strategy for local
    - Eventual goal will probably be to just do away with Passport completely

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,26 @@
 const auth = require('express').Router();
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('../models/user');
 
 // Generic passport serialization & deserialization
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
 });
 
-passport.deserializeUser((id, done) => {
+passport.deserializeUser(function(id, done) {
   User.findById(id)
-  .then(user => done(null, user))
-  .catch(err => done(err));
+  .then(user => {
+    done(null, user);
+  })
+  .catch(done);
 });
 
-// Local signup
 auth.post('/local/signup', (req, res, next) => {
   User.create(req.body)
   .then(user => {
-    req.login(user, err => {
+    req.logIn(user, err => {
       if (err) next(err);
       else res.status(201).json(user);
     });
@@ -28,34 +28,29 @@ auth.post('/local/signup', (req, res, next) => {
   .catch(next);
 });
 
-// Local login
 auth.post('/local/login', (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/api/auth/whoami'
-  })(req, res, next);
-});
-
-// Local login strategy
-// Use email as the username
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-}, (email, password, done) => {
-  User.findOne({ email }).exec()
+  const { email, password } = req.body;
+  User.findOne({ email })
   .then(user => {
     if (!user) {
-      return done(null, false, { message: 'Incorrect login information.' });
+      res.status(401).json({ success: false, message: 'Incorrect login information.' });
+    } else {
+      return user.authenticate(password)
+      .then(ok => {
+        if (!ok) {
+          res.status(401).res.json({ success: false, message: 'Incorrect login information.' });
+        } else {
+          req.logIn(user, err => {
+            if (err) return next(err);
+            return res.json(user);
+          });
+        }
+      })
+      .catch(next);
     }
-    return user.authenticate(password)
-    .then(ok => {
-      if (!ok) {
-        return done(null, false, { message: 'Incorrect login information.' });
-      }
-      done(null, user);
-    });
   })
-  .catch(done);
-}));
+  .catch(next);
+});
 
 // Send user info front-end
 auth.get('/whoami', (req, res, next) => {
@@ -65,7 +60,7 @@ auth.get('/whoami', (req, res, next) => {
 
 // Logout
 auth.post('/logout', (req, res, next) => {
-  req.logout();
+  req.logOut();
   res.redirect('/api/auth/whoami');
 });
 
